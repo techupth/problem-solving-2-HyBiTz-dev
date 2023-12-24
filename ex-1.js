bdee4367cd5da297d9c9035c462269f2e55a423d
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-12-24 19:36:00 +0700

    Finish Problem Solving exercise

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,25 @@
 //Exercise 1: Roman to Integer
 
-const romanToInt = function (s) {
+const romanNumber = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+const romanToInt = function (value) {
   //Start coding here
+  let result = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (romanNumber[value[i]] < romanNumber[value[i + 1]]) {
+      result += romanNumber[value[i + 1]] - romanNumber[value[i]];
+      i++;
+    } else {
+      result += romanNumber[value[i]];
+    }
+  }
+  return result;
 };
 
 const result1 = romanToInt("III"); // 3
 const result2 = romanToInt("LVIII"); // 58
 const result3 = romanToInt("MCMXCIV"); //1994
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,19 @@
 
 const fizzBuzz = function (n) {
   //Start coding here
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      result.push(`FizzBuzz`);
+    } else if (i % 5 == 0) {
+      result.push(`Buzz`);
+    } else if (i % 3 == 0) {
+      result.push(`Fizz`);
+    } else {
+      result.push(`${i}`);
+    }
+  }
+  return result;
 };
 
 const result1 = fizzBuzz(3);
